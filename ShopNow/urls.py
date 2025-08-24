8ced93762d48ec9183bf06b159a1511c29d27f39
anshul <anshulsahu729@gmail.com>
2025-08-24 09:11:26 +0530

    Sucessfully creates the folder structure

---
+++ ShopNow/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('blog/', include('blog.urls')),
+    path('cart/', include('cart.urls')),
+    path('dashboard/', include('dashboard.urls')),
+    path('newsletter/', include('newsletter.urls')),
+    path('store/', include('store.urls')),
+]
---
+++ newsletter/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+
+urlpatterns = [
+    # Add newsletter views here
+]
