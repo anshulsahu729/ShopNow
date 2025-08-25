55326e76e01d840580058c457daabc43a6b9de8c
Manas <goojrem@gmail.com>
2025-08-25 13:57:38 +0530

    blog is working

--- blog/forms.py
+++ blog/forms.py
@@ -4,10 +4,12 @@ from .models import BlogPost
 class BlogPostForm(forms.ModelForm):
     class Meta:
         model = BlogPost
-        fields = ['title', 'body', 'image', 'active']
+        fields = ['title', 'slug', 'body', 'image', 'published_date', 'active']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'slug': forms.TextInput(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control', 'rows': 6}),
             'image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'published_date': forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime-local'}),
             'active': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
         }
--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
 
-app_name = 'blog'  # ← add this
+app_name = 'blog'
 
 urlpatterns = [
     path('', views.blog_list, name='blog_list'),
-    path('create/', views.blog_create, name='blog_create'),
     path('<slug:slug>/', views.blog_detail, name='blog_detail'),
-    path('<slug:slug>/edit/', views.blog_update, name='blog_update'),
-    path('<slug:slug>/delete/', views.blog_delete, name='blog_delete'),
+    path('create/new/', views.blog_create, name='blog_create'),
+    path('update/<slug:slug>/', views.blog_update, name='blog_update'),
+    path('delete/<slug:slug>/', views.blog_delete, name='blog_delete'),
 ]
